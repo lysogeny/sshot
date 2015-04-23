f4e3742319756ba9b3957b0ad5c01ff256ff65d6
Jooa Hooli <jooa.hooli@uni-konstanz.de>
2015-04-23 21:57:31 +0200

    Added a script for uploading a file specified in options

---
+++ upload.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Take a file and upload it to a server via ssh
+server=jooa.me
+remoteuser=jooa
+wwwdir=/var/www/
+target=u/
+scp $@ $remoteuser@$server:$wwwdir$target$name
+
+remotefile=$server'/'$target$@
+echo $remotefile | xsel -bi
+notify-send $remotefile
+
